621be517cbdec1e72bc75bd937cd8a360a730731
Ishan Jaidka <ijaidka@gmail.com>
2023-01-28 00:12:48 -0800

    add tile images

---
+++ src/components/images/index.js
@@ -0,0 +1,7 @@
+import blank from "./blank.png";
+import lava from "./lava.png";
+import mud from "./mud.png";
+import speeder from "./speeder.png";
+import person from "./person.png";
+
+export { blank, lava, mud, speeder, person };
