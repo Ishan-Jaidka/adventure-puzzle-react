75fe54b6e4746fdb94c5db771fdf6d95a5c3a8b8
Ishan Jaidka <ijaidka@gmail.com>
2023-01-31 01:11:28 -0800

    css

--- src/components/loss.js
+++ src/components/loss.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./loss.css";
 
 export default function Loss() {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className="outerDiv">
       You Lost! Try again?
       <button
+        className="button"
         onClick={() => {
           navigate("/game");
         }}
--- src/components/start.js
+++ src/components/start.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./start.css";
 
 export default function Start() {
   const navigate = useNavigate();
   return (
-    <div>
-      Welcome! Play a game?
-      <button
-        onClick={() => {
-          navigate("game");
-        }}
-      >
-        Start
-      </button>
+    <div className="outerDiv">
+      <h1>Welcome to the Adventure Game!</h1>
+      <h2>Play a game?</h2>
+      <div className="buttonDiv">
+        <button
+          className="button"
+          onClick={() => {
+            navigate("game");
+          }}
+        >
+          Start
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            navigate("instructions");
+          }}
+        >
+          Instructions
+        </button>
+      </div>
     </div>
   );
 }
